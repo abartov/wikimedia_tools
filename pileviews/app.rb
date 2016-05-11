fdbcb5b1e0bf583157b68522d5fd2470401fe1a4
Asaf Bartov <asaf.bartov@gmail.com>
2016-05-11 02:10:17 +0000

    Now available as a Sinatra tool

--- pileviews/app.rb
+++ pileviews/app.rb
@@ -1,11 +1,24 @@
 # app.rb
 
 require 'sinatra'
+require 'tempfile'
+require './pileviewslib'
+
 configure do
   set :bind, '0.0.0.0'
   set :port, ENV['PILEVIEWS_PORT']
 end
-get '/' do
-  'Hello world!'
+get '/pileviews' do
+  haml :index
 end
 
+post '/pileviews/make_csv' do
+  fname = "pileviews_#{params[:pile_id]}"
+  temp_file = Tempfile.new(fname)
+  temp_file.write(pileviews_csv(params[:pile_id]))
+  temp_file.close # NOTE: file will be automatically deleted when temp_file is garbage-collected
+  send_file temp_file.path, filename: fname+'.csv', type: 'text/csv'
+#  content_type 'text/csv'
+#  views = pileviews_csv(params[:pile_id])
+  #send_data views, filename: "pagepile_#{params[:pile_id]}.csv", type: 'text/csv'
+end
